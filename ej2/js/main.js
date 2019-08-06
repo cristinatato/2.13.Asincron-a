0bed8c1837375f2df753b5837c7960b1eb3e590b
cristinatato <c.tatodelatorre@gmail.com>
2019-08-06 09:02:04 +0200

    add ej2

---
+++ ej2/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const container= document.querySelector('.container');
+console.log(container);
+let counter = 0;
+let temp;
+
+const incrementAndShowCounter = () => {
+  counter++;
+  const time = document.querySelector('.time');
+  time.innerHTML = counter;
+  if (counter === 12) {
+    clearInterval(temp);
+  }
+  container.innerHTML += `
+  <img class="uva" src="https://www.foodretail.es/2017/12/28/shoppers/uvas-mandan-Nochevieja-espanola_1177092288_306091_660x372.jpg">
+  `;
+};
+
+temp = setInterval(incrementAndShowCounter, 1000);
